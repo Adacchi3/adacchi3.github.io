782ffd6a75b1c94efc0478b8f004962c9a52b508
Adacchi3 <adachi.prog@gmail.com>
2021-04-29 17:31:03 +0900

    [chore] add i18n

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ const withPreact = require('next-plugin-preact')
 
 module.exports = withPreact({
   /* regular next.js config options here */
+  i18n: {
+    locales: ['ja', 'en'],
+    defaultLocale: 'ja'
+  }
 })
