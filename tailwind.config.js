42a77a8e0b2e629d8f699cd71a1c00da35a60f18
Adacchi3 <adachi.prog@gmail.com>
2021-04-29 15:57:25 +0900

    [chore] setup prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   mode: 'jit',
-  purge: [
-    'src/pages/**/*.{ts,tsx}',
-    'src/components/**/*.{ts,tsx}',
-  ],
+  purge: ['src/pages/**/*.{ts,tsx}', 'src/components/**/*.{ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
