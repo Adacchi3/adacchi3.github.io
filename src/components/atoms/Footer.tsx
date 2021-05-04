7e66b3ac32a1b5e1f7fb54fdac369a28b053d9d8
Adacchi3 <adachi.prog@gmail.com>
2021-05-04 22:31:17 +0900

    [style] add mt-4

--- src/components/atoms/Footer.tsx
+++ src/components/atoms/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer: React.VFC = () => {
   return (
-    <footer className="mt-auto bg-gray-100 p-2">
+    <footer className="mt-auto bg-gray-100 p-2 mt-4">
       <p className="text-center text-sm text-gray-600">© 2017 Adacchi3</p>
     </footer>
   )
