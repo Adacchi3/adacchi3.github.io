e13b9f21dff327b0095da136d40e44cc6d3b639b
Adacchi3 <adachi.prog@gmail.com>
2021-05-05 03:12:45 +0900

    [feat] add get static props and path in [slug]

--- src/pages/articles/[slug]/index.tsx
+++ src/pages/articles/[slug]/index.tsx
@@ -1,5 +1,30 @@
 import React from 'react'
 import PostTemplate from '@components/templates/PostTemplate'
+import { GetStaticPaths, GetStaticProps } from 'next'
+
+type StaticProps = {
+  params: {
+    slug: string
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({
+  params,
+}: StaticProps) => {
+  const slug = params.slug
+  return {
+    props: {
+      slug,
+    },
+  }
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: ['/articles/hoge', '/articles/fuga'],
+    fallback: false,
+  }
+}
 
 const Post: React.VFC = () => {
   return <PostTemplate />
