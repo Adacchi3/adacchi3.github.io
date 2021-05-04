8452e0c28b4b935bbc2d5a162848cedca917face
Adacchi3 <adachi.prog@gmail.com>
2021-05-05 04:31:36 +0900

    [feat] setup GTM

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
   env: {
     GRAPHQL_ENDPOINT: process.env.GRAPHQL_ENDPOINT,
     PREVIEW: process.env.NODE_ENV === 'production' ? false : true,
-    AUTHOR_ID: process.env.AUTHOR_ID
+    AUTHOR_ID: process.env.AUTHOR_ID,
+    GTM_ID: process.env.GTM_ID,
   }
 }
