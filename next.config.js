4e569c58c94935e01d8a07d6180d15941be1b5e9
Adacchi3 <adachi.prog@gmail.com>
2026-03-15 14:53:28 +0900

    Add @apollo/client to serverExternalPackages to fix react-server module resolution

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   output: 'export',
+  serverExternalPackages: ['@apollo/client'],
   env: {
     GRAPHQL_ENDPOINT: process.env.GRAPHQL_ENDPOINT,
     PREVIEW: process.env.NODE_ENV === 'production' ? 'false' : 'true',
